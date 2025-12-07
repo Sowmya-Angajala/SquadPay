408edef97b488bc55abfd77c0c7030f39d46ad33
Sowmya-Angajala <angajalasowmya02@gmail.com>
2025-12-07 19:37:56 +0530

    host changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,21 @@
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: process.env.DB_DIALECT || "mysql",
+//     logging: false,
+//   }
+// );
+
+// module.exports = sequelize;
+
+
 const { Sequelize } = require("sequelize");
-require("dotenv").config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -7,9 +23,17 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || "mysql",
-    logging: false,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false
   }
 );
 
 module.exports = sequelize;
+
